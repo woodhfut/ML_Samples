5b27085e9392f843683b01faa27a1bde32fdf66e
woodhfut <woodhfut@hotmail.com>
2019-03-21 17:02:48 +0800

    modify test sample
    modify test sample

--- CAPTCHARecognize.py
+++ CAPTCHARecognize.py
@@ -4,6 +4,9 @@ import os
 from sklearn import neighbors
 from sklearn import svm
 import time
+import re
+from sklearn import metrics
+
 
 m = 3278
 width, height = 20, 40
@@ -16,21 +19,23 @@ y=[] #img labels vector
 
 ImgSize = (width,height)
 
-RootPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'samples')
+RootPath = os.path.dirname(os.path.abspath(__file__))
+TrainingDataPath = os.path.join(RootPath, 'samples')
+TestingDataPath = os.path.join(RootPath, 'Imgs')
 
 def img2vect(imgPath):
     try:
         img = Image.open(imgPath)
-        return np.asanyarray(img).flatten()
+        return np.asanyarray(img).ravel()
     except IOError as ex:
         print('error for {}, ignore it.{}'.format(imgPath, ex))
 
 
-def prepareData(X, y, rootpath):
-    print('rootpath is ', rootpath)
+def prepareTrainingData(X, y, rootpath):
+    print('training data rootpath is ', rootpath)
     count = 0
     for d in os.listdir(rootpath):
-        tmp = os.path.join(os.path.join(RootPath, d), 'resized')
+        tmp = os.path.join(os.path.join(TrainingDataPath, d), 'resized')
         for f in os.listdir(tmp):
             imgdata = img2vect(os.path.join(tmp, f))
 
@@ -38,7 +43,7 @@ def prepareData(X, y, rootpath):
             y.append(d) 
             count +=1
 
-def getTestData(imgPath):
+def prepareTestData(imgPath):
     try:
         im = Image.open(imgPath)
         im = im.convert('P')
@@ -90,7 +95,7 @@ def getTestData(imgPath):
         for letter in letters:
             im3 = im2.crop((letter[0], 0, letter[1], im2.size[1]))
             im3 = im3.resize(ImgSize)
-            testX[count] = np.asarray(im3).flatten()
+            testX[count] = np.asarray(im3).ravel()
             count+=1
 
         return testX
@@ -98,20 +103,38 @@ def getTestData(imgPath):
         print('error: ', ex)
 
 if __name__ =='__main__':
-    prepareData(X, y, RootPath)
+    prepareTrainingData(X, y, TrainingDataPath)
     
+    X_test =[]
+    y_test=[]
+    for d in os.listdir(TestingDataPath):
+        if re.match(r'[A-Za-z]{4}.*', d):
+            print('d is ', d)
+            y_test.extend(d[0:4].lower())
+            x = prepareTestData(os.path.join(TestingDataPath, d))
+            X_test.extend(x)
+
+    print(y_test)
+
     t = time.time()
     knn = neighbors.KNeighborsClassifier()
     
     knn.fit(X, y)
-    testx = getTestData('./Imgs/2.png')
-    r = knn.predict(testx)
+    
+    knn_pre = knn.predict(X_test)
 
-    print('knn: result is {}, time used {}sec'.format(r, time.time()-t))
+    knn_accuracy = metrics.accuracy_score(y_test, knn_pre)
+
+    print(knn_pre)
+    print('knn: accuracy is {}, time used {}sec'.format(knn_accuracy, time.time()-t))
 
     t = time.time()
     svc = svm.SVC(gamma='scale', decision_function_shape='ovo', C=1, verbose=False)
 
     svc.fit(X, y)
-    pre = svc.predict(testx)
-    print('svm: result is {}, time used {}sec'.format(pre, time.time() -t))
+    svm_pre = svc.predict(X_test)
+
+    svm_accuracy = metrics.accuracy_score(y_test, svm_pre)
+
+    print(svm_pre)
+    print('svm: accuracy is {}, time used {}sec'.format(svm_accuracy, time.time() -t))
